cdb3f11b34226938ab6e17fa6dea1f7c4f6f1282
oni <selaka.onila@gmail.com>
2023-03-13 13:43:25 +0530

    list view

--- App.tsx
+++ App.tsx
@@ -1,117 +1,99 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  Image,
+  FlatList,
+  ScrollView,
+  StyleSheet,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+interface Product {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+}
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const products: Product[] = [
+  {
+    id: 1,
+    title: 'Product 1',
+    image: 'https://via.placeholder.com/150',
+    price: 10.99,
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    image: 'https://via.placeholder.com/150',
+    price: 19.99,
+  },
+  // add more products here
+];
+
+interface ProductItemProps {
+  title: string;
+  image: string;
+  price: number;
+}
+
+const ProductItem = ({title, image, price}: ProductItemProps) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.productItemContainer}>
+      <Image source={{uri: image}} style={styles.productItemImage} />
+      <Text style={styles.productItemTitle}>{title}</Text>
+      <Text style={styles.productItemPrice}>${price.toFixed(2)}</Text>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const MarketplaceScreen = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView>
+      <View style={styles.productListContainer}>
+        <FlatList
+          data={products}
+          renderItem={({item}) => (
+            <ProductItem
+              title={item.title}
+              image={item.image}
+              price={item.price}
+            />
+          )}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  productListContainer: {
+    padding: 10,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  productItemContainer: {
+    backgroundColor: '#f5f5f5',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  productItemImage: {
+    width: 80,
+    height: 80,
+    marginRight: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  productItemTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  productItemPrice: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginLeft: 'auto',
   },
 });
 
-export default App;
+export default MarketplaceScreen;
